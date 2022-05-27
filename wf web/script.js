b3758a872f0ad7a146aa6a92f5bafe1643ba9b46
Cass <congodsted@gmail.com>
2022-05-27 18:08:48 -0400

    WF2 draft and Download Guide

--- wf web/script.js
+++ wf web/script.js
@@ -2,6 +2,7 @@ window.onload = document.getElementById("header").scrollIntoView();
 document.querySelector(`#homebutton`).setAttribute("class", "activebutton");
     function openmods() {
         document.querySelector(`#home`).style.display = "none";
+        document.querySelector(`#downloads`).style.display = "none";
         document.querySelector(`#changelog`).style.display = "none";
         document.querySelector(`#modlist`).style.display = "block";
         document.querySelector(`#homebutton`).setAttribute("class", "inactivebutton");
@@ -11,6 +12,7 @@ document.querySelector(`#homebutton`).setAttribute("class", "activebutton");
     function openhome() {
         document.querySelector(`#modlist`).style.display = "none";
         document.querySelector(`#changelog`).style.display = "none";
+        document.querySelector(`#downloads`).style.display = "none";
         document.querySelector(`#home`).style.display = "block";
         document.querySelector(`#modsbutton`).setAttribute("class", "inactivebutton");
         document.querySelector(`#homebutton`).setAttribute("class", "activebutton");
@@ -20,11 +22,31 @@ document.querySelector(`#homebutton`).setAttribute("class", "activebutton");
     function openchangelog() {
         document.querySelector(`#modlist`).style.display = "none";
         document.querySelector(`#home`).style.display = "none";
+        document.querySelector(`#downloads`).style.display = "none";
         document.querySelector(`#changelog`).style.display = "block";
         document.querySelector(`#homebutton`).setAttribute("class", "inactivebutton");
         document.querySelector(`#changelogbutton`).setAttribute("class", "activebutton");
         document.querySelector(`#modsbutton`).setAttribute("class", "inactivebutton");
         
+    }
+
+    function opendownloads() {
+        document.querySelector(`#modlist`).style.display = "none";
+        document.querySelector(`#home`).style.display = "none";
+        document.querySelector(`#downloads`).style.display = "block";
+        document.querySelector(`#changelog`).style.display = "none";
+        document.querySelector(`#homebutton`).setAttribute("class", "inactivebutton");
+        document.querySelector(`#changelogbutton`).setAttribute("class", "inactivebutton");
+        document.querySelector(`#downloadbutton`).setAttribute("class", "activebutton");
+        document.querySelector(`#modsbutton`).setAttribute("class", "inactivebutton");
+        
+    }
+
+    function copyURL() {
+        var copyText = document.getElementById("packurl");
+        copyText.select(); 
+        copyText.setSelectionRange(0, 99999); /* For mobile devices */
+        navigator.clipboard.writeText(copyText.value);
     }
 
     var coll = document.getElementsByClassName("collapsible");
